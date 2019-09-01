d4fa382d5d40742db8cc77ee28d05dacbca0a714
Emil K <kaae@techie.com>
2019-09-01 12:07:50 +0200

    Update setCookie.js

--- setCookie.js
+++ setCookie.js
@@ -1,5 +1,4 @@
 function setCookie(cookieName, cookieDomain, cookieUrl, cookieVal) {
-    // console.log(cookieName,cookieDomain, cookieVal,cookieUrl);
     chrome.cookies.set({
         "name": cookieName,
         "domain": cookieDomain,
@@ -13,7 +12,24 @@ function setCookie(cookieName, cookieDomain, cookieUrl, cookieVal) {
 }
 
 function parseCookie(cookie) {
+    if (cookie['domain'] == "www.starcowparis.com") cookie['domain'] = ".www.starcowparis.com"
     setCookie(cookie['name'], cookie['domain'], cookie['url'], cookie['value']);
+}
+
+function delCookie(cookie) {
+    if(cookie['domain'].includes("paypal")) return;
+    chrome.cookies.getAll({domain: cookie['domain']}, function(cookies) {
+        for(var i=0; i<cookies.length;i++) {
+            if (cookies[i].domain.startsWith(".")) {
+                url = "www"+cookies[i].domain;
+            } else if (cookies[i].domain.startsWith("www")) {
+                url = "https://"+cookies[i].domain;
+            } else {
+                url = cookies[i].domain;
+            }
+            chrome.cookies.remove({url:  + cookies[i].path, name: cookies[i].name});
+        }
+    });
 }
 
 
@@ -34,10 +50,16 @@ function init() {
             var redirect = atob(url.searchParams.get("redirect").toString());
 
             for (var cookie of cookies) {
+                delCookie(cookie)
+            }
+
+            for (var cookie of cookies) {
                 parseCookie(cookie)
             }
 
-            document.write('<body style="background-color: #0a1c2e"><center><img src="kaiserlogo.png" height="150" width="150"><h1 style="color: green">Cookies set, redirecting to ' + redirect + '</h1></center></body>');
+
+
+            document.write('<body style="background-color: #0a1c2e"><center><h1 style="color: green">Cookies set, redirecting to ' + redirect + '</h1></center></body>');
             //update tab with redirect param url
             chrome.tabs.query({
                 currentWindow: true,
@@ -47,8 +69,6 @@ function init() {
                     url: redirect
                 });
             });
-        } else {
-            document.write('<body style="background-color: #0a1c2e"><center><img src="kaiserlogo.png" height="150" width="150"><h1 style="color: red">Only works with payment links from KaiserFramework</h1></center></body>');
         }
     });
 }
